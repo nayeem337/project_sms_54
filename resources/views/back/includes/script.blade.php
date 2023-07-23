435f276ae9ddc0e262b768b1309755b03da68c0d
Jannatul Nayeem <123932013+nayeem337@users.noreply.github.com>
2023-07-23 09:33:28 +0600

    first commit

---
+++ resources/views/back/includes/script.blade.php
@@ -0,0 +1,14 @@
+<!-- JAVASCRIPT -->
+<script src="{{ asset('/') }}back/assets/libs/jquery/jquery.min.js"></script>
+<script src="{{ asset('/') }}back/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('/') }}back/assets/libs/metismenu/metisMenu.min.js"></script>
+<script src="{{ asset('/') }}back/assets/libs/simplebar/simplebar.min.js"></script>
+<script src="{{ asset('/') }}back/assets/libs/node-waves/waves.min.js"></script>
+
+<!-- apexcharts -->
+<script src="{{ asset('/') }}back/assets/libs/apexcharts/apexcharts.min.js"></script>
+
+<script src="{{ asset('/') }}back/assets/js/pages/dashboard.init.js"></script>
+
+<!-- App js -->
+<script src="{{ asset('/') }}back/assets/js/app.js"></script>
